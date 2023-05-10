32813f060f591ed33aac4fd88959b83674152efc
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-05-09 21:57:04 -0500

    ORM Reading

--- src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
+++ src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
@@ -1,13 +1,16 @@
 package org.launchcode.enumerableplanets.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.launchcode.enumerableplanets.data.Planets;
 
 @Controller
 public class PlanetController {
 
     @GetMapping
-    public String displayIndex() {
+    public String displayIndex(Model model) {
+        model.addAttribute("planets", Planets.values());
         return "index";
     }
 }
